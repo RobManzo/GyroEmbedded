20b9053646fd812e9dbe0e7ba27fe4d0d44a8626
RobManzo <roberto.manzo01@community.unipa.it>
2022-05-09 14:34:26 +0200

    HEArthv2

---
+++ temp.f
@@ -0,0 +1,199 @@
+\***NUMBERS***\
+: N_1 ( buffer color -- buffer color ) SWAP 1 PR
+    DUP 1 PD 3 3 PICK VERT_LINE         1 PR
+    DUP 4 3 PICK VERT_LINE                  
+    DUP C PD 2 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE                  
+    DUP C PD 2 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE                  
+    DUP C PD 2 3 PICK VERT_LINE         1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP C PD 2 3 PICK VERT_LINE         1 PR
+    DUP C PD 2 3 PICK VERT_LINE         1 PR
+    DUP C PD 2 3 PICK VERT_LINE         1 PR
+    NEXT_CHAR SWAP ;
+: N_2 ( buffer color -- buffer color ) SWAP 1 PR
+    DUP 2 PD 2 3 PICK VERT_LINE         1 PR
+    DUP 1 PD 3 3 PICK VERT_LINE         
+    DUP A PD 4 3 PICK VERT_LINE         1 PR
+    DUP 4 3 PICK VERT_LINE                  
+    DUP 9 PD 5 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE                  
+    DUP 8 PD 6 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE                  
+    DUP 7 PD 7 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE                  
+    DUP 6 PD 8 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE                  
+    DUP 5 PD 5 3 PICK VERT_LINE         
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE                  
+    DUP 4 PD 5 3 PICK VERT_LINE         
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 8 3 PICK VERT_LINE                  
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 1 PD 6 3 PICK VERT_LINE
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 2 PD 4 E PICK VERT_LINE
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    NEXT_CHAR SWAP ;
+: N_3 ( buffer color -- buffer color ) SWAP 1 PR
+    DUP 3 3 PICK VERT_LINE         
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE         
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE         
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE
+    DUP 6 PD 2 3 PICK VERT_LINE         
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE
+    DUP 6 PD 2 3 PICK VERT_LINE          
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE
+    DUP 5 PD 4 3 PICK VERT_LINE          
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP 1 PD 5 3 PICK VERT_LINE
+    DUP 8 PD 5 3 PICK VERT_LINE         1 PR
+    DUP 2 PD 3 3 PICK VERT_LINE
+    DUP 9 PD 3 3 PICK VERT_LINE         1 PR
+    NEXT_CHAR SWAP ;
+: N_4 ( buffer color -- buffer color ) SWAP 1 PR
+    DUP 6 PD 5 3 PICK VERT_LINE         1 PR
+    DUP 5 PD 6 3 PICK VERT_LINE         1 PR
+    DUP 4 PD 7 3 PICK VERT_LINE         1 PR
+    DUP 3 PD 4 3 PICK VERT_LINE         
+    DUP 8 PD 3 3 PICK VERT_LINE         1 PR
+    DUP 2 PD 4 3 PICK VERT_LINE         
+    DUP 8 PD 3 3 PICK VERT_LINE         1 PR
+    DUP 1 PD 4 3 PICK VERT_LINE         
+    DUP 8 PD 3 3 PICK VERT_LINE         1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP 8 PD 3 3 PICK VERT_LINE         1 PR
+    NEXT_CHAR SWAP ;
+: N_5 ( buffer color -- buffer color ) SWAP 1 PR
+    DUP 7 3 PICK VERT_LINE         
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 8 3 PICK VERT_LINE         
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 5 PD 4 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 5 PD 4 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 5 PD 4 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 5 PD 4 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 5 PD 4 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 5 PD 4 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE         
+    DUP 6 PD 8 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE         
+    DUP 7 PD 6 3 PICK VERT_LINE         1 PR
+    NEXT_CHAR SWAP ;
+: N_6 ( buffer color -- buffer color ) SWAP 1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 6 PD 3 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 6 PD 3 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 6 PD 3 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 6 PD 3 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 4 3 PICK VERT_LINE         
+    DUP 6 PD 8 3 PICK VERT_LINE         1 PR
+    DUP 4 3 PICK VERT_LINE         
+    DUP 6 PD 8 3 PICK VERT_LINE         1 PR
+    DUP 4 3 PICK VERT_LINE         
+    DUP 6 PD 8 3 PICK VERT_LINE         1 PR
+    NEXT_CHAR SWAP ;
+: N_7 ( buffer color -- buffer color ) SWAP 1 PR
+    DUP 3 3 PICK VERT_LINE              1 PR
+    DUP 3 3 PICK VERT_LINE              
+    DUP C PD 2 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE              
+    DUP A PD 4 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE              
+    DUP 8 PD 6 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE              
+    DUP 6 PD 8 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE              
+    DUP 4 PD 8 3 PICK VERT_LINE         1 PR
+    DUP A 3 PICK VERT_LINE              1 PR
+    DUP 8 3 PICK VERT_LINE              1 PR
+    DUP 6 3 PICK VERT_LINE              1 PR
+    DUP 4 3 PICK VERT_LINE              1 PR
+    NEXT_CHAR SWAP ;
+: N_8 ( buffer color -- buffer color ) SWAP 1 PR
+    DUP 2 PD 2 3 PICK VERT_LINE              
+    DUP A PD 2 3 PICK VERT_LINE         1 PR
+    DUP 1 PD 4 3 PICK VERT_LINE              
+    DUP 9 PD 4 3 PICK VERT_LINE         1 PR
+    DUP 6 3 PICK VERT_LINE              
+    DUP 8 PD 6 3 PICK VERT_LINE         1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP 2 3 PICK VERT_LINE  
+    DUP 5 PD 4 3 PICK VERT_LINE       
+    DUP C PD 2 3 PICK VERT_LINE         1 PR
+    DUP 2 3 PICK VERT_LINE  
+    DUP 5 PD 4 3 PICK VERT_LINE       
+    DUP C PD 2 3 PICK VERT_LINE         1 PR
+    DUP 2 3 PICK VERT_LINE  
+    DUP 5 PD 4 3 PICK VERT_LINE       
+    DUP C PD 2 3 PICK VERT_LINE         1 PR
+    DUP 2 3 PICK VERT_LINE  
+    DUP 5 PD 4 3 PICK VERT_LINE       
+    DUP C PD 2 3 PICK VERT_LINE         1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP 6 3 PICK VERT_LINE              
+    DUP 8 PD 6 3 PICK VERT_LINE         1 PR
+    DUP 1 PD 4 3 PICK VERT_LINE              
+    DUP 9 PD 4 3 PICK VERT_LINE         1 PR
+    DUP 2 PD 2 3 PICK VERT_LINE              
+    DUP A PD 2 3 PICK VERT_LINE         1 PR
+    NEXT_CHAR SWAP ;
+: N_9 ( buffer color -- buffer color ) SWAP 1 PR
+    DUP 8 3 PICK VERT_LINE              
+    DUP A PD 4 3 PICK VERT_LINE         1 PR
+    DUP 8 3 PICK VERT_LINE              
+    DUP A PD 4 3 PICK VERT_LINE         1 PR
+    DUP 8 3 PICK VERT_LINE              
+    DUP A PD 4 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 5 PD 3 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 5 PD 3 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 5 PD 3 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP 3 3 PICK VERT_LINE  
+    DUP 5 PD 3 3 PICK VERT_LINE       
+    DUP B PD 3 3 PICK VERT_LINE         1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    DUP E 3 PICK VERT_LINE              1 PR
+    NEXT_CHAR SWAP ;
